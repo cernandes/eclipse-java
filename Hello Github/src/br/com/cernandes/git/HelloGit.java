7a3a3da7680174168b7f09a3f1982ec3e53c3ec1
Claudio Ernandes Silva <53997849+cernandes@users.noreply.github.com>
2020-08-25 09:43:50 -0300

    adicionado arquivos via upload

---
+++ Hello Github/src/br/com/cernandes/git/HelloGit.java
@@ -0,0 +1,13 @@
+package br.com.cernandes.git;
+/**
+ * @author Claudio
+ * Exemplo de integração da IDE Eclipse com o Github
+ * */
+public class HelloGit {
+
+	public static void main(String[] args) {
+	System.out.println("Hello Git");
+
+	}
+
+}
